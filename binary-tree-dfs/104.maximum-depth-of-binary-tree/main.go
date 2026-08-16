85448faef3d8317cebdef692712815ecceac8e31
unomns <unomns@gmail.com>
2026-08-16 23:38:37 +0800

    maximum-depth-of-binary-tree

---
+++ binary-tree-dfs/104.maximum-depth-of-binary-tree/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+/**
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
+Example 1:
+	Input: root = [3,9,20,null,null,15,7]
+	Output: 3
+
+Example 2:
+	Input: root = [1,null,2]
+	Output: 2
+
+
+Constraints:
+	The number of nodes in the tree is in the range [0, 104].
+	-100 <= Node.val <= 100
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	t := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 17,
+			},
+		},
+	}
+
+	fmt.Println(maxDepth(t))
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if right > left {
+		return right + 1
+	}
+	return left + 1
+}
