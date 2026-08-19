d101be2d3369db174c1ce51eb805f1db499f9740
unomns <unomns@gmail.com>
2026-08-19 11:05:09 +0800

    binary-tree-right-side-view

---
+++ binary-tree/bfs/199.binary-tree-right-side-view/main.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+/*
+Given the root of a binary tree, imagine yourself standing on the right side of it,
+return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+	Input: root = [1,2,3,null,5,null,4]
+	Output: [1,3,4]
+
+Example 2:
+	Input: root = [1,2,3,4,null,null,null,5]
+	Output: [1,3,4,5]
+
+Example 3:
+	Input: root = [1,null,3]
+	Output: [1,3]
+
+Example 4:
+	Input: root = []
+	Output: []
+
+
+Constraints:
+	The number of nodes in the tree is in the range [0, 100].
+	-100 <= Node.val <= 100
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	tree := &TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 3,
+				},
+				Right: &TreeNode{
+					Val: -2,
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: -3,
+			Right: &TreeNode{
+				Val: 11,
+			},
+		},
+	}
+	/*
+				   10
+				/      \
+			   5       -3
+			  /  \     /
+		     3    2   11
+		    / \    \
+		   3  -2    1
+
+		   output: 10, -3, 11, 1
+	*/
+	fmt.Println(rightSideView(tree))
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	nums := []int{}
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := range levelSize {
+			curr := queue[0]
+			queue = queue[1:]
+
+			if i == 0 {
+				nums = append(nums, curr.Val)
+			}
+
+			if curr.Right != nil {
+				queue = append(queue, curr.Right)
+			}
+			if curr.Left != nil {
+				queue = append(queue, curr.Left)
+			}
+		}
+	}
+
+	return nums
+}
