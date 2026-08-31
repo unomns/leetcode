794cdb6c06d9f842545392f8b0ae2f393796cbcf
unomns <unomns@gmail.com>
2026-08-31 22:10:43 +0800

    successful-pairs-of-spells-and-potions

---
+++ binary-search/2300.successful-pairs-of-spells-and-potions/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+/**
+You are given two positive integer arrays spells and potions, of length n and m respectively,
+where spells[i] represents the strength of the ith spell and potions[j] represents the strength of the jth potion.
+
+You are also given an integer success.
+A spell and potion pair is considered successful if the product of their strengths is at least success.
+
+Return an integer array pairs of length n
+where pairs[i] is the number of potions that will form a successful pair with the ith spell.
+
+
+Example 1:
+	Input: spells = [5,1,3], potions = [1,2,3,4,5], success = 7
+	Output: [4,0,3]
+	Explanation:
+	- 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+	- 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+	- 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
+	Thus, [4,0,3] is returned.
+
+Example 2:
+	Input: spells = [3,1,2], potions = [8,5,8], success = 16
+	Output: [2,0,2]
+	Explanation:
+	- 0th spell: 3 * [8,5,8] = [24,15,24]. 2 pairs are successful.
+	- 1st spell: 1 * [8,5,8] = [8,5,8]. 0 pairs are successful.
+	- 2nd spell: 2 * [8,5,8] = [16,10,16]. 2 pairs are successful.
+	Thus, [2,0,2] is returned.
+
+
+Constraints:
+	n == spells.length
+	m == potions.length
+	1 <= n, m <= 105
+	1 <= spells[i], potions[i] <= 105
+	1 <= success <= 1010
+*/
+
+func main() {
+	fmt.Println(successfulPairs([]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, 7))
+	fmt.Println(successfulPairs([]int{3, 1, 2}, []int{8, 5, 8}, 16))
+}
+
+func successfulPairs(spells []int, potions []int, success int64) []int {
+	res := make([]int, len(spells))
+
+	slices.Sort(potions)
+
+	for i, spell := range spells {
+		min, max := 0, len(potions)-1
+		idx := len(potions)
+
+		requiredPotion := (success + int64(spell) - 1) / int64(spell)
+
+		for min <= max {
+			mid := min + (max-min)/2
+
+			if int64(potions[mid]) >= requiredPotion {
+				idx = mid
+				max = mid - 1
+			} else {
+				min = mid + 1
+			}
+		}
+
+		res[i] = len(potions) - idx
+	}
+
+	return res
+}
