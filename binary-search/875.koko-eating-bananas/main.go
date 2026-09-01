0f9812dc0de6b3753e43a1d5d8737ef2b62fc56f
unomns <unomns@gmail.com>
2026-09-01 17:07:02 +0800

    koko-eating-bananas

---
+++ binary-search/875.koko-eating-bananas/main.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+/**
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas.
+The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k.
+Each hour, she chooses some pile of bananas and eats k bananas from that pile.
+If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+
+Example 1:
+	Input: piles = [3,6,7,11], h = 8
+	Output: 4
+
+Example 2:
+	Input: piles = [30,11,23,4,20], h = 5
+	Output: 30
+
+Example 3:
+	Input: piles = [30,11,23,4,20], h = 6
+	Output: 23
+
+
+Constraints:
+	1 <= piles.length <= 104
+	piles.length <= h <= 109
+	1 <= piles[i] <= 109
+*/
+
+func main() {
+	fmt.Println(minEatingSpeed([]int{3, 6, 7, 11}, 8))       // 4
+	fmt.Println(minEatingSpeed([]int{30, 11, 23, 4, 20}, 5)) // 30
+	fmt.Println(minEatingSpeed([]int{30, 11, 23, 4, 20}, 6)) // 23
+}
+
+func minEatingSpeed(piles []int, h int) int {
+	max := piles[0]
+	for _, p := range piles {
+		if p > max {
+			max = p
+		}
+	}
+	min := 1
+
+	for min < max {
+		mid := min + (max-min)/2
+
+		hours := 0
+		for _, p := range piles {
+			hours += (p + mid - 1) / mid
+		}
+
+		if hours <= h {
+			max = mid
+		} else {
+			min = mid + 1
+		}
+	}
+
+	return min
+}
