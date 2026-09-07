d251d2bfb6d0a2e6b2ebe547452c5f888971b202
unomns <unomns@gmail.com>
2026-09-07 14:55:27 +0800

    search-suggestions-system

---
+++ trie/1268.search-suggestions-system/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/**
+You are given an array of strings products and a string searchWord.
+
+Design a system that suggests at most three product names from products after each character of searchWord is typed.
+Suggested products should have common prefix with searchWord.
+If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+Return a list of lists of the suggested products after each character of searchWord is typed.
+
+
+Example 1:
+	Input: products = ["mobile","mouse","moneypot","monitor","mousepad"], searchWord = "mouse"
+	Output: [["mobile","moneypot","monitor"],["mobile","moneypot","monitor"],["mouse","mousepad"],["mouse","mousepad"],["mouse","mousepad"]]
+	Explanation: products sorted lexicographically = ["mobile","moneypot","monitor","mouse","mousepad"].
+	After typing m and mo all products match and we show user ["mobile","moneypot","monitor"].
+	After typing mou, mous and mouse the system suggests ["mouse","mousepad"].
+
+Example 2:
+	Input: products = ["havana"], searchWord = "havana"
+	Output: [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+	Explanation: The only word "havana" will be always suggested while typing the search word.
+
+
+Constraints:
+	1 <= products.length <= 1000
+	1 <= products[i].length <= 3000
+	1 <= sum(products[i].length) <= 2 * 104
+	All the strings of products are unique.
+	products[i] consists of lowercase English letters.
+	1 <= searchWord.length <= 1000
+	searchWord consists of lowercase English letters.
+*/
+
+func main() {
+	fmt.Println(suggestedProducts([]string{"mobile", "mouse", "moneypot", "monitor", "mousepad"}, "mouse"))
+}
+
+func suggestedProducts(products []string, searchWord string) [][]string {
+	type Node struct {
+		children [26]*Node
+		words    []string
+	}
+
+	sort.Strings(products)
+
+	root := &Node{}
+
+	for _, w := range products {
+		curr := root
+		for _, c := range w {
+			idx := int(c - 'a')
+			if curr.children[idx] == nil {
+				curr.children[idx] = &Node{}
+			}
+			curr = curr.children[idx]
+			if len(curr.words) < 3 {
+				curr.words = append(curr.words, w)
+			}
+		}
+	}
+
+	res := make([][]string, len(searchWord))
+	curr := root
+	for i, c := range searchWord {
+		idx := int(c - 'a')
+		if curr != nil {
+			curr = curr.children[idx]
+		}
+
+		if curr != nil {
+			res[i] = curr.words
+		} else {
+			res[i] = []string{}
+		}
+	}
+
+	return res
+}
