5d96014ada28b602450058945c69801b8dfac50d
unomns <unomns@gmail.com>
2026-09-25 08:46:33 +0800

    valid-sudoku

---
+++ matrix/36.valid-sudoku/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+Determine if a 9 x 9 Sudoku board is valid.
+
+Only the filled cells need to be validated according to the following rules:
+1. Each row must contain the digits 1-9 without repetition.
+2. Each column must contain the digits 1-9 without repetition.
+3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+- A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+- Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+	Input: board =
+	[["5","3",".",".","7",".",".",".","."]
+	,["6",".",".","1","9","5",".",".","."]
+	,[".","9","8",".",".",".",".","6","."]
+	,["8",".",".",".","6",".",".",".","3"]
+	,["4",".",".","8",".","3",".",".","1"]
+	,["7",".",".",".","2",".",".",".","6"]
+	,[".","6",".",".",".",".","2","8","."]
+	,[".",".",".","4","1","9",".",".","5"]
+	,[".",".",".",".","8",".",".","7","9"]]
+	Output: true
+
+Example 2:
+	Input: board =
+	[["8","3",".",".","7",".",".",".","."]
+	,["6",".",".","1","9","5",".",".","."]
+	,[".","9","8",".",".",".",".","6","."]
+	,["8",".",".",".","6",".",".",".","3"]
+	,["4",".",".","8",".","3",".",".","1"]
+	,["7",".",".",".","2",".",".",".","6"]
+	,[".","6",".",".",".",".","2","8","."]
+	,[".",".",".","4","1","9",".",".","5"]
+	,[".",".",".",".","8",".",".","7","9"]]
+	Output: false
+	Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+	board.length == 9
+	board[i].length == 9
+	board[i][j] is a digit 1-9 or '.'.
+*/
+
+func main() {
+	fmt.Println(isValidSudoku([][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	})) // true
+
+	fmt.Println(isValidSudoku([][]byte{
+		{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	})) // false
+}
+
+func isValidSudoku(board [][]byte) bool {
+	var boxes, horz, vert [9][9]bool
+
+	for y := range 9 {
+		for x := range 9 {
+			curr := board[y][x]
+			if curr == '.' {
+				continue
+			}
+
+			digit := curr - '1'
+			boxIdx := y/3*3 + (x / 3)
+
+			if vert[y][digit] || horz[x][digit] || boxes[boxIdx][digit] {
+				return false
+			}
+
+			vert[y][digit] = true
+			horz[x][digit] = true
+			boxes[boxIdx][digit] = true
+		}
+	}
+
+	return true
+}
